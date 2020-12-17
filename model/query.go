5d3365ada72dd25b36df444cb539bda92fbba080
MitsuhaOma <2391542095@qq.com>
2020-12-17 19:29:47 +0800

    :art: add the part of parsing queryinfo

--- model/query.go
+++ model/query.go
@@ -9,6 +9,13 @@ type QueryResponse struct {
 	Info string `json:"info"`
 }
 
+type Sentence struct {
+	Source        string `json:"source"`
+	Target        string `json:"target"`
+	RawPredicates string `json:"rawPredicates"`
+	Predicates    string `json:"predicates"`
+}
+
 func QueryInfo(query string, limit, offset int) ([]*QueryResponse, error) {
 	var res []*QueryResponse
 	raw, err := service.QueryInfo(query, limit, offset)
@@ -20,5 +27,13 @@ func QueryInfo(query string, limit, offset int) ([]*QueryResponse, error) {
 		return res, err
 	}
 
+	return res, nil
+}
+
+func ParseInfo(info string) ([]*Sentence, error) {
+	var res []*Sentence
+	if  err := json.Unmarshal([]byte(info), &res); err != nil {
+		return nil, err
+	}
 	return res, nil
 }
